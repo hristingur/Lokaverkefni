cd1e166c8aeef43e3483eea0374aeb4f8fb3cb84
hristingur <eibbi_b@hotmail.com>
2017-04-27 16:34:35 +0000

    Add files via upload
    lagaði insert skipun og breytti contact lookinu. insert virkar nuna, hægt að senda feedback

--- insert.php
+++ insert.php
@@ -12,7 +12,7 @@
 		$username = "1611943609";
 		$password = "mypassword";
 		$db = "1611943609_lokaverkonn3";
-
+echo" <p>"."konnqqqqq"."</p>";
 		// Create connection
 		$conn = new mysqli($servername, $username, $password, $db);
 
@@ -21,21 +21,39 @@
 		    die("Tenging Mistókst: " . $conn->connect_error);
 		}
 
+		print_r($_REQUEST);
+		$name = $_REQUEST['name'];
+		$netfang = $_REQUEST['email'];
+		$feedback = $_REQUEST['feedback'];
+		echo "<p>"."nafnid er ".$name."</p>";
+		echo" <p>"."netfangi er ".$netfang."</p>";
+		echo" <p>".$feedback."</p>";
 
-		$netfang = isset($_REQUEST['netfang']);
-		$feedback = isset($_REQUEST['feedback']);
+		$sql = "INSERT INTO vaelubillinn(name,email,feedback) VALUES ('$name','$netfang','$feedback')";
+		if (mysqli_query($conn, $sql)) {
+    		echo "New record created successfully";
+		} else {
+    		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
 
-		//print($netfang);debug
 
-		$sql = "INSERT INTO vaelubillinn (netfang,feedback) VALUES ('$netfang','$feedback')";
-		$result = $conn->query($sql);
+
+
+
+
+
+
+		//$sql = "INSERT INTO vaelubillinn(name,netfang,feedback) VALUES ('$name','$netfang','$feedback')";
+		//$result = $conn->query($sql);
+		//print_r($result);
 
 		$conn -> close();
 
 		sleep(1); //tefja um eina sek
-		header("Location: index.html"); //förum á aðalsíðu
-		exit();
 
+		//header("Location: index.html"); //förum á aðalsíðu
+		exit();
+			echo "$netfang";
 
  	?>
 
