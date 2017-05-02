988a3eab38e7c71dd366f298f18ca6b2500bcb99
hristingur <eibbi_b@hotmail.com>
2017-05-02 10:47:07 +0000

    Add files via upload
    Vann mikið i að klara about us.html og css og thad er eins og er skipt i 3 dalka og thar verdur myndir af okkur og utskyrt hvad er gif og fleira

---
+++ about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About us</title>
+	<link rel="stylesheet" type="text/css" href="about.css">
+</head>
+<body>
+	<div class="banner">
+		<h1>About us</h1>
+	</div>
+</body>
+</html>
